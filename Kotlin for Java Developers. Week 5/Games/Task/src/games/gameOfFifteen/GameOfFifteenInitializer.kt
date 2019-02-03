7e4035d453a9e4ddb156345bfcc564136b742126
Shubhm Jain <jshubham2304@gmail.com>
2019-02-03 17:34:23 +0530

    Write the code

---
+++ Kotlin for Java Developers. Week 5/Games/Task/src/games/gameOfFifteen/GameOfFifteenInitializer.kt
@@ -0,0 +1,17 @@
+package games.gameOfFifteen
+
+interface GameOfFifteenInitializer {
+    /*
+     * Even permutation of numbers 1..15
+     * used to initialized first 15 cells on a board
+     * (the last cell is empty)
+     */
+    val initialPermutation: List<Int>
+}
+
+class RandomGameInitializer : GameOfFifteenInitializer {
+    override val initialPermutation by lazy {
+        TODO()
+    }
+}
+
